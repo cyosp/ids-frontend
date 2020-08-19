863e8bad97f1f5732973edf4a0f1bd601d499c82
CYOSP <cyosp@cyosp.com>
2020-08-19 07:28:37 +0200

    Add Jwt access denied handler

---
+++ src/main/java/com/cyosp/ids/security/JwtAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package com.cyosp.ids.security;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        httpServletResponse.sendError(SC_FORBIDDEN, accessDeniedException.getMessage());
+    }
+}
