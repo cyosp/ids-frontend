b4a8008bf7b06bdb332afc3084b92dbb60b3ffed
CYOSP <cyosp@cyosp.com>
2020-08-19 07:29:28 +0200

    Add Jwt authentication entry point

---
+++ src/main/java/com/cyosp/ids/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.cyosp.ids.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException authenticationException) throws IOException {
+        httpServletResponse.sendError(SC_UNAUTHORIZED, authenticationException.getMessage());
+    }
+}
