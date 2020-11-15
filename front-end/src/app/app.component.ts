242d4b814ccb4352e1b91a2a812702593c91173f
CYOSP <cyosp@cyosp.com>
2020-11-15 09:32:21 +0100

    Display gallery navigation only in gallery view

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -11,6 +11,7 @@ import {Router} from '@angular/router';
 export class AppComponent implements OnInit {
     title = 'Ids';
     isAuthenticated = false;
+    isGallery = false;
     directories: any[] = [];
 
     constructor(private tokenStorageService: TokenStorageService,
@@ -18,29 +19,31 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.isAuthenticated = this.tokenStorageService.hasTokenNonExpired();
 
         this.router.events.subscribe(
             val => {
+                this.isAuthenticated = this.tokenStorageService.hasTokenNonExpired();
+                this.isGallery = this.router.url.startsWith('/gallery');
                 this.directories = [];
-
-                let urlPath = decodeURI(this.router.url).replace(/\/gallery/, '');
-                if (urlPath.startsWith('/')) {
-                    urlPath = urlPath.substring(1);
-                }
-                if ( urlPath !== '')
-                {
-                    let directoryId = '';
-                    const folders = urlPath.split('>');
-                    for (let i = 0; i < folders.length; i++) {
-                        const directoryName = folders[i];
-                        directoryId += (i === 0 ? '/' : '>') + directoryName;
-                        this.directories = this.directories.concat(
-                            {
-                                id: directoryId,
-                                name: directoryName
-                            }
-                        );
+                if (this.isGallery) {
+                    let urlPath = decodeURI(this.router.url).replace(/\/gallery/, '');
+                    if (urlPath.startsWith('/')) {
+                        urlPath = urlPath.substring(1);
+                    }
+                    if ( urlPath !== '')
+                    {
+                        let directoryId = '';
+                        const folders = urlPath.split('>');
+                        for (let i = 0; i < folders.length; i++) {
+                            const directoryName = folders[i];
+                            directoryId += (i === 0 ? '/' : '>') + directoryName;
+                            this.directories = this.directories.concat(
+                                {
+                                    id: directoryId,
+                                    name: directoryName
+                                }
+                            );
+                        }
                     }
                 }
             }
