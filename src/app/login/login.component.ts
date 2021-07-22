4e335defcbf72e24e179c2f9e815cd8913066fc8
CYOSP <cyosp@cyosp.com>
2021-07-22 08:17:30 +0200

    Pretty print update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,55 +5,55 @@ import {ListQuery} from '../list-query.service';
 import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  form: any = {};
-  isAuthenticated = false;
-  isLoginFailed = false;
-  view;
+    form: any = {};
+    isAuthenticated = false;
+    isLoginFailed = false;
+    view;
 
-  constructor(private tokenStorageService: TokenStorageService,
-              private authenticationService: AuthenticationService,
-              private router: Router,
-              private userListQuery: ListQuery,
-              private route: ActivatedRoute) {
-  }
+    constructor(private tokenStorageService: TokenStorageService,
+                private authenticationService: AuthenticationService,
+                private router: Router,
+                private userListQuery: ListQuery,
+                private route: ActivatedRoute) {
+    }
 
-  ngOnInit(): void {
-      this.isAuthenticated = this.tokenStorageService.hasTokenNonExpired();
-      this.route.queryParams.subscribe(params => {
-          if (params.view) {
-              this.view = decodeURI(params.view);
-          }
-      });
-  }
+    ngOnInit(): void {
+        this.isAuthenticated = this.tokenStorageService.hasTokenNonExpired();
+        this.route.queryParams.subscribe(params => {
+            if (params.view) {
+                this.view = decodeURI(params.view);
+            }
+        });
+    }
 
-  onSubmit(): void {
-    this.authenticationService.login(this.form).subscribe(
-      dataRest => {
-        this.tokenStorageService.saveToken(dataRest.accessToken);
-        this.isAuthenticated = true;
-        this.isLoginFailed = false;
-        if (this.view) {
-          this.router.navigate([this.view]);
-        } else {
-          this.router.navigate(['gallery']);
-        }
+    onSubmit(): void {
+        this.authenticationService.login(this.form).subscribe(
+            dataRest => {
+                this.tokenStorageService.saveToken(dataRest.accessToken);
+                this.isAuthenticated = true;
+                this.isLoginFailed = false;
+                if (this.view) {
+                    this.router.navigate([this.view]);
+                } else {
+                    this.router.navigate(['gallery']);
+                }
 
-        this.userListQuery.fetch()
-          .subscribe(({data}) => console.log(data));
-      },
-      () => {
-        this.isLoginFailed = true;
-      }
-    );
-  }
+                this.userListQuery.fetch()
+                    .subscribe(({data}) => console.log(data));
+            },
+            () => {
+                this.isLoginFailed = true;
+            }
+        );
+    }
 
-  reloadPage(): void {
-    window.location.reload();
-  }
+    reloadPage(): void {
+        window.location.reload();
+    }
 }
